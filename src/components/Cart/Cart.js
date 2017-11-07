2060a18a1556af41eae47d49b9460416af5f3339
Steven Bready <stevenbready@MacBook-Pro.local>
2017-11-07 13:50:10 -0700

    Completed Mini Project

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { Link } from 'react-router-dom';
 
 import Product from './Product/Product';
 import ShoppingCart from 'react-icons/lib/fa/shopping-cart';
@@ -13,11 +14,11 @@ class Cart extends Component {
       expanded: false
     };
 
-    this.toggleCartDetails = this.toggleCartDetails.bind( this );
+    this.toggleCartDetails = this.toggleCartDetails.bind(this);
   }
-  
+
   toggleCartDetails() {
-    this.setState({ expanded: !this.state.expanded});
+    this.setState({ expanded: !this.state.expanded });
   }
 
   render() {
@@ -27,43 +28,45 @@ class Cart extends Component {
       howMuchSwag
     } = this.props;
 
-    let swagComponents = swagInCart.map( swag => (
-      <Product 
-        key={ swag.id }
-        title={ swag.title }
-        img={ swag.img }
-        id={ swag.id }
+    let swagComponents = swagInCart.map(swag => (
+      <Product
+        key={swag.id}
+        title={swag.title}
+        img={swag.img}
+        id={swag.id}
       />
     ));
 
     return (
-      this.state.expanded 
-      ?
+      this.state.expanded
+        ?
         <div id="Cart__containerExpanded">
-          <div id="Cart__header" onClick={ this.toggleCartDetails }>
-            <span> <ShoppingCart id="Cart__icon" /> { howMuchSwag } items: </span>
-            <span> ${ total } </span>
+          <div id="Cart__header" onClick={this.toggleCartDetails}>
+            <span> <ShoppingCart id="Cart__icon" /> {howMuchSwag} items: </span>
+            <span> ${total} </span>
           </div>
           <div id="Cart__details">
-            { swagComponents }
+            {swagComponents}
           </div>
-          <div id="Cart__footer" onClick={ this.toggleCartDetails }>
-            <span> Checkout </span>
-          </div>
+          <Link to='/checkout' id='Cart_navLink'>
+            <div id="Cart__footer" onClick={this.toggleCartDetails}>
+              <span> Checkout </span>
+            </div>
+          </Link>
         </div>
-      :
-        <div id="Cart__container" onClick={ this.toggleCartDetails }>
-          <span> <ShoppingCart id="Cart__icon" /> { howMuchSwag } items: </span>
-          <span> ${ total } </span>
+        :
+        <div id="Cart__container" onClick={this.toggleCartDetails}>
+          <span> <ShoppingCart id="Cart__icon" /> {howMuchSwag} items: </span>
+          <span> ${total} </span>
         </div>
     )
   }
 }
 
-function mapStateToProps( state ) {
-  var swagInCart = state.cart.map( id => state.swag.filter( swag => swag.id === id )[0] );
-  var total = swagInCart.reduce( ( total, swag ) => total + parseFloat(swag.price), 0 ).toFixed(2);
+function mapStateToProps(state) {
+  var swagInCart = state.cart.map(id => state.swag.filter(swag => swag.id === id)[0]);
+  var total = swagInCart.reduce((total, swag) => total + parseFloat(swag.price), 0).toFixed(2);
   return { swagInCart, total, howMuchSwag: swagInCart.length };
 }
 
-export default connect( mapStateToProps )( Cart );
+export default connect(mapStateToProps)(Cart);
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,29 +1,35 @@
 import React from "react";
 import { connect } from "react-redux";
+import { Link } from 'react-router-dom';
+
 
 import './Nav.css';
 
 import ShoppingCart from 'react-icons/lib/fa/shopping-cart';
 
-function Nav( { items } ) {
+function Nav({ items }) {
   return (
     <div id="Nav__container">
       <div id="Nav__linksContainer">
-        <span className="Nav__label">Store</span>
+        <Link className='navLink' to='/'>
+          <span className="Nav__label">Store</span>
+        </Link>
 
-        <div id="Nav__checkoutContainer">
-          <div id="Nav__itemsInCart"> { items } </div>
-          <ShoppingCart id="Nav__shoppingCart" />
-        </div>
+        <Link className='navLink' to='/checkout'>
+          <div id="Nav__checkoutContainer">
+            <div id="Nav__itemsInCart"> {items} </div>
+            <ShoppingCart id="Nav__shoppingCart" />
+          </div>
+        </Link>
       </div>
     </div>
   )
 }
 
-function mapStateToProps( state ) {
+function mapStateToProps(state) {
   return {
     items: state.cart.length
   };
 }
 
-export default connect( mapStateToProps )( Nav );
+export default connect(mapStateToProps)(Nav);
